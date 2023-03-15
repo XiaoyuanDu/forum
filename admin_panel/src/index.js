08df3023c0452f2997f9c28a5c158e30a9f2bd6f
杜晓源 <duxiaoyuan@duxiaoyuandeMacBook-Pro.local>
2023-03-16 00:52:26 +0800

    docker

--- admin_panel/src/index.js
+++ admin_panel/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "assets/css/App.css";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import AuthLayout from "layouts/auth";
-import AdminLayout from "layouts/admin";
+import AdminLayout from "./layouts/admin";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "theme/theme";
+import theme from "./theme/theme";
 import { ThemeEditorProvider } from "@hypertheme-editor/chakra-ui";
 
 ReactDOM.render(
